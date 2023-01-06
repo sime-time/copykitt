2839c4ee57407b7fad9b9b27c76e85c00fdbeded
Dunn, Simeon M <simedunn@iu.edu>
2023-01-05 20:16:05 -0500

    text completion test

--- app/copykitt.py
+++ app/copykitt.py
@@ -2,7 +2,8 @@ import os
 import openai 
 import argparse
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# Load API key from environment variable
+openai.api_key = os.getenv("OPENAI_API_KEY") 
 
 def main():
   print("Running Main!")
@@ -13,19 +14,22 @@ def main():
   user_input = args.input 
 
   print(f"User input: {user_input}")
+  generate_branding_snippet(user_input)
   pass
 
-subject = "coffee"
-prompt = f"Generate upbeat branding snippet for {subject}"
+def generate_branding_snippet(arg_prompt: str):
+  # create a prompt for GPT 
+  prompt = f"Short upbeat branding snippet for {arg_prompt}: "
 
-""" response = openai.Completion.create(
-  model = "davinci-instruct-beta-v3",
-  prompt = prompt,
-  temperature = 0,
-  max_tokens = 32
-)
+  # access GPT text completion using prompt
+  response = openai.Completion.create(
+    model = "davinci-instruct-beta-v3",
+    prompt = prompt,
+    temperature = 0,
+    max_tokens = 32
+  )
 
-print(response) """
+  print(response)
 
 if __name__ == "__main__":
   main()
