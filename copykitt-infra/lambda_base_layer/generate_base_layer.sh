54109670893f4f3c2dc86c6b99feeb9d323ea00f
Dunn, Simeon M <simedunn@iu.edu>
2023-01-18 10:56:00 -0500

    generate base layer

--- copykitt-infra/lambda_base_layer/generate_base_layer.sh
+++ copykitt-infra/lambda_base_layer/generate_base_layer.sh
@@ -1,6 +1,13 @@
-# generate a base layer for the Lambda functions
+# Generates a base layer for the Lambda functions
 
 # remove the container first (if it exists)
 docker rm layer-container 
 
 # build the base layer
+docker build -t base-layer . 
+
+# rename to layer-container
+docker run --name layer-container base-layer 
+
+# copy the generated zip artifact so our CDK can use it 
+docker cp layer-container:layer.zip . && echo "Created layer.zip with updated base layer"
