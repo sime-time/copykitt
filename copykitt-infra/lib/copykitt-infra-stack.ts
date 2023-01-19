23dc12d99ebefd1845b603ed6923d9ae4329d2e0
Dunn, Simeon M <simedunn@iu.edu>
2023-01-18 20:54:22 -0500

    aws lambdone

--- copykitt-infra/lib/copykitt-infra-stack.ts
+++ copykitt-infra/lib/copykitt-infra-stack.ts
@@ -1,15 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dotenv from "dotenv";
+
+dotenv.config()
 
 export class CopykittInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // add the zip file with all python dependency code 
+    const layer = new lambda.LayerVersion(this, "BaseLayer", {
+      code: lambda.Code.fromAsset("lambda_base_layer/layer.zip"),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_9]
+    })
+
+    // add the python code
     const apiLambda = new lambda.Function(this, "ApiFunction", {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset("../app/"),
-      handler: "copykitt_api.handler"
+      handler: "copykitt_api.handler",
+      layers: [layer],
+      environment: {
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY ?? ""
+      }
     })
   }
 }
