e84a776ac46f877fd73d1ece960822d22c3c8144
Dunn, Simeon M <simedunn@iu.edu>
2023-01-25 21:24:15 -0500

    api lambda integration

--- copykitt-infra/lib/copykitt-infra-stack.ts
+++ copykitt-infra/lib/copykitt-infra-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apiGateway from "aws-cdk-lib/aws-apigateway";
 import * as dotenv from "dotenv";
 
 dotenv.config()
@@ -13,7 +14,7 @@ export class CopykittInfraStack extends cdk.Stack {
     const layer = new lambda.LayerVersion(this, "BaseLayer", {
       code: lambda.Code.fromAsset("lambda_base_layer/layer.zip"),
       compatibleRuntimes: [lambda.Runtime.PYTHON_3_9]
-    })
+    });
 
     // add the python code
     const apiLambda = new lambda.Function(this, "ApiFunction", {
@@ -24,6 +25,18 @@ export class CopykittInfraStack extends cdk.Stack {
       environment: {
         OPENAI_API_KEY: process.env.OPENAI_API_KEY ?? ""
       }
-    })
+    });
+
+    // create an instance of a REST api
+    const copyKittApi = new apiGateway.RestApi(this, "RestApi", {
+      restApiName: "CopyKitt API"
+    });
+
+    // create a proxy integration for copykittapi to connect to lambda
+    const lambdaApiIntegration = new apiGateway.LambdaIntegration(apiLambda);
+    copyKittApi.root.addProxy({
+      defaultIntegration: lambdaApiIntegration
+    });
+
   }
 }
